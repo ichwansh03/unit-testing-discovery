aac89ee6c4ea25cdf5f9855e5bfe742fc3b34c06
ichwansh03 <ichwansholihin03@gmail.com>
2024-02-29 10:33:25 +0700

    add total table

--- src/main/java/com/ichwan/springseries/dto/RoomClassRequest.java
+++ src/main/java/com/ichwan/springseries/dto/RoomClassRequest.java
@@ -1,6 +1,7 @@
 package com.ichwan.springseries.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
-public record RoomClassRequest(@NotBlank String code) {
+public record RoomClassRequest(@NotBlank String code, @NotNull int totalTable) {
 }
